7d5c860010169dbb0574e4fe6e37f84c74084c00
Joey Chen <jay.hatelove@gmail.com>
2021-04-27 21:17:12 +0800

    refactor: rename variable "temp" to "currentDate"

--- src/main/java/Finance.java
+++ src/main/java/Finance.java
@@ -7,56 +7,55 @@ import java.util.List;
 
 public class Finance {
 
-	private IBudgetRepo repo;
+    private IBudgetRepo repo;
 
-	public Finance(IBudgetRepo repo) {
-		this.repo = repo;
-	}
+    public Finance(IBudgetRepo repo) {
+        this.repo = repo;
+    }
 
-	public double queryBudget(LocalDate start, LocalDate end) {
-		if(end.isBefore(start)){
-			return 0;
-		}
-		List<Budget> data = repo.getAll();
-		double amount = 0;
+    public double queryBudget(LocalDate start, LocalDate end) {
+        if (end.isBefore(start)) {
+            return 0;
+        }
+        List<Budget> data = repo.getAll();
+        double amount = 0;
 
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMM");
-		String formattedStart = start.format(formatter);
-		String formattedEnd = end.format(formatter);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMM");
+        String formattedStart = start.format(formatter);
+        String formattedEnd = end.format(formatter);
 
-		if(formattedStart.equals(formattedEnd)){
-			long days = start.until(end, ChronoUnit.DAYS) + 1;
-			amount = notFunnAmount(days, start.lengthOfMonth(),data,formattedStart);
-		} else{
-			LocalDate temp =  LocalDate.of(start.getYear(), start.getMonthValue(), 1); // 20211001
-			while (true) {
-				String tempFormat = temp.format(formatter);
-				if(tempFormat.equals(formattedStart)) {
-					int days = start.lengthOfMonth() - start.getDayOfMonth() +1;
-					amount += notFunnAmount(days, start.lengthOfMonth(),data, tempFormat);
-					temp = temp.plusMonths(1);
-				} else if(tempFormat.equals(formattedEnd)){
-					int days = end.getDayOfMonth();
-					amount += notFunnAmount(days, end.lengthOfMonth(),data, tempFormat);
-					break;
-				}else {
-					int days = temp.lengthOfMonth();
-					amount += notFunnAmount(days, temp.lengthOfMonth(),data, tempFormat);
-					temp = temp.plusMonths(1);
-				}
-			}
-		}
+        if (formattedStart.equals(formattedEnd)) {
+            long days = start.until(end, ChronoUnit.DAYS) + 1;
+            amount = notFunnAmount(days, start.lengthOfMonth(), data, formattedStart);
+        } else {
+            LocalDate currentDate = LocalDate.of(start.getYear(), start.getMonthValue(), 1); // 20211001
+            while (true) {
+                String tempFormat = currentDate.format(formatter);
+                if (tempFormat.equals(formattedStart)) {
+                    int days = start.lengthOfMonth() - start.getDayOfMonth() + 1;
+                    amount += notFunnAmount(days, start.lengthOfMonth(), data, tempFormat);
+                    currentDate = currentDate.plusMonths(1);
+                } else if (tempFormat.equals(formattedEnd)) {
+                    int days = end.getDayOfMonth();
+                    amount += notFunnAmount(days, end.lengthOfMonth(), data, tempFormat);
+                    break;
+                } else {
+                    int days = currentDate.lengthOfMonth();
+                    amount += notFunnAmount(days, currentDate.lengthOfMonth(), data, tempFormat);
+                    currentDate = currentDate.plusMonths(1);
+                }
+            }
+        }
 
-		return amount;
+        return amount;
+    }
 
-	}
-
-	private double notFunnAmount(long days,  int lengthOfMonth, List<Budget> data, String formattedDate) {
-		for(Budget budget: data) {
-			if(formattedDate.equals(budget.yearMonth)) {
-				return  budget.amount/lengthOfMonth * days;
-			}
-		}
-		return 0;
-	}
+    private double notFunnAmount(long days, int lengthOfMonth, List<Budget> data, String formattedDate) {
+        for (Budget budget : data) {
+            if (formattedDate.equals(budget.yearMonth)) {
+                return budget.amount / lengthOfMonth * days;
+            }
+        }
+        return 0;
+    }
 }
--- src/test/java/BudgetTest.java
+++ src/test/java/BudgetTest.java
@@ -9,90 +9,86 @@ import java.util.Arrays;
 import static java.util.Arrays.asList;
 import static org.mockito.Mockito.when;
 
-
 public class BudgetTest {
 
-	private Finance finance;
-	private IBudgetRepo repo;
+    private Finance finance;
+    private IBudgetRepo repo;
 
-	@BeforeEach
-	void setUp() {
-		repo = Mockito.mock(IBudgetRepo.class);
-		finance = new Finance(repo);
-	}
+    @Test
+    public void fullMonth() {
+        when(repo.getAll()).thenReturn(asList(new Budget("202110", 31)));
 
-	@Test
-	public void fullMonth(){
-		when(repo.getAll()).thenReturn(asList(new Budget("202110",31)));
+        LocalDate start = LocalDate.of(2021, 10, 1);
+        LocalDate end = LocalDate.of(2021, 10, 31);
 
-		LocalDate start = LocalDate.of(2021, 10, 1);
-		LocalDate end = LocalDate.of(2021, 10, 31);
+        Assertions.assertEquals(31, finance.queryBudget(start, end));
+    }
 
-		Assertions.assertEquals(31  , finance.queryBudget(start, end));
-	}
+    @Test
+    public void notfullMonth() {
+        when(repo.getAll()).thenReturn(asList(new Budget("202110", 31)));
 
+        LocalDate start = LocalDate.of(2021, 10, 3);
+        LocalDate end = LocalDate.of(2021, 10, 10);
 
-	@Test
-	public void notfullMonth(){
-		when(repo.getAll()).thenReturn(asList(new Budget("202110",31)));
+        Assertions.assertEquals(8, finance.queryBudget(start, end));
+    }
 
-		LocalDate start = LocalDate.of(2021, 10, 3);
-		LocalDate end = LocalDate.of(2021, 10, 10);
+    @Test
+    public void twoMonth() {
+        when(repo.getAll()).thenReturn(asList(
+                new Budget("202110", 31),
+                new Budget("202111", 30)
+        ));
 
-		Assertions.assertEquals(8  , finance.queryBudget(start, end));
-	}
+        LocalDate start = LocalDate.of(2021, 10, 30);
+        LocalDate end = LocalDate.of(2021, 11, 10);
 
-	@Test
-	public void twoMonth(){
-		when(repo.getAll()).thenReturn(asList(
-		new Budget("202110",31),
-		new Budget("202111",30)
-		));
+        Assertions.assertEquals(12, finance.queryBudget(start, end));
+    }
 
-		LocalDate start = LocalDate.of(2021, 10, 30);
-		LocalDate end = LocalDate.of(2021, 11, 10);
+    @Test
+    public void twoMonth2() {
+        when(repo.getAll()).thenReturn(asList(
+                new Budget("202111", 30),
+                new Budget("202112", 31)
+        ));
 
-		Assertions.assertEquals(12  , finance.queryBudget(start, end));
-	}
+        LocalDate start = LocalDate.of(2021, 10, 30);
+        LocalDate end = LocalDate.of(2021, 12, 10);
 
-	@Test
-	public void twoMonth2(){
-		when(repo.getAll()).thenReturn(asList(
-		new Budget("202111",30),
-		new Budget("202112",31)
-		));
+        Assertions.assertEquals(40, finance.queryBudget(start, end));
+    }
 
-		LocalDate start = LocalDate.of(2021, 10, 30);
-		LocalDate end = LocalDate.of(2021, 12, 10);
+    @Test
+    public void year2() {
+        when(repo.getAll()).thenReturn(asList(
+                new Budget("202112", 31),
+                new Budget("202202", 28)
+        ));
 
-		Assertions.assertEquals(40  , finance.queryBudget(start, end));
-	}
+        LocalDate start = LocalDate.of(2021, 12, 30);
+        LocalDate end = LocalDate.of(2022, 2, 3);
 
-	@Test
-	public void year2(){
-		when(repo.getAll()).thenReturn(asList(
-		new Budget("202112",31),
-		new Budget("202202",28)
-		));
+        Assertions.assertEquals(5, finance.queryBudget(start, end));
+    }
 
-		LocalDate start = LocalDate.of(2021, 12, 30);
-		LocalDate end = LocalDate.of(2022, 2, 3);
+    @Test
+    public void dateError() {
+        when(repo.getAll()).thenReturn(asList(
+                new Budget("202112", 31),
+                new Budget("202202", 28)
+        ));
 
-		Assertions.assertEquals(5, finance.queryBudget(start, end));
-	}
+        LocalDate start = LocalDate.of(2021, 12, 30);
+        LocalDate end = LocalDate.of(2021, 11, 3);
 
-	@Test
-	public void dateError(){
-		when(repo.getAll()).thenReturn(asList(
-		new Budget("202112",31),
-		new Budget("202202",28)
-		));
+        Assertions.assertEquals(0, finance.queryBudget(start, end));
+    }
 
-		LocalDate start = LocalDate.of(2021, 12, 30);
-		LocalDate end = LocalDate.of(2021, 11, 3);
-
-		Assertions.assertEquals(0, finance.queryBudget(start, end));
-	}
-
-
+    @BeforeEach
+    void setUp() {
+        repo = Mockito.mock(IBudgetRepo.class);
+        finance = new Finance(repo);
+    }
 }
