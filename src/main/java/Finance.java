2fea5ad60632e9f0d494efcd921631c1d528489b
Joey Chen <jay.hatelove@gmail.com>
2021-04-27 22:03:20 +0800

    refactor: clean up

--- src/main/java/Finance.java
+++ src/main/java/Finance.java
@@ -1,8 +1,4 @@
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-
-import static java.time.format.DateTimeFormatter.ofPattern;
-import static java.time.temporal.ChronoUnit.DAYS;
 
 public class Finance {
 
@@ -18,23 +14,10 @@ public class Finance {
         }
         double amount = 0;
 
-        DateTimeFormatter formatter = ofPattern("yyyyMM");
-        String formattedStart = start.format(formatter);
-        String formattedEnd = end.format(formatter);
-
         Period period = new Period(start, end);
         for (Budget budget : repo.getAll()) {
             amount += budget.overlappingAmount(period);
         }
-//        if (formattedStart.equals(formattedEnd)) {
-//            long days = start.until(end, DAYS) + 1;
-//            amount = overlappingAmount(days, formattedStart);
-//        } else {
-//            Period period = new Period(start, end);
-//            for (Budget budget : repo.getAll()) {
-//                amount += budget.overlappingAmount(period);
-//            }
-//        }
 
         return amount;
     }
